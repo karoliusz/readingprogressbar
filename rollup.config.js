fa6e7a07c66973f67a35a54a525beabeba94e59c
karoliusz <karoliusz@gmail.com>
2022-04-25 20:09:39 +0200

    Added new bundle type

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 import scss from 'rollup-plugin-scss'
+import { terser } from 'rollup-plugin-terser'
+import { defineConfig } from 'rollup';
 
 const name = require('./package.json').main.replace(/\.js$/, '')
 
-export default [
-  {
+const configs = [
+  defineConfig({
     input: 'src/index.ts',
     external: ['rxjs', 'rxjs/operators'],
     plugins: [
@@ -22,10 +24,28 @@ export default [
         file: `${name}.mjs`,
         format: 'es',
         sourcemap: true,
-      },
+      }
     ],
-  },
-  {
+  }),
+  defineConfig({
+    input: 'src/index.ts',
+    external: ['./scss/main.scss'],
+    plugins: [
+      esbuild({
+        minify: true,
+        target: 'es2017'
+      }),
+    ],
+    output: [
+      {
+        file: `${name}.min.js`,
+        format: 'iife',
+        sourcemap: true,
+        name: 'ReadingProgressBar'
+      }
+    ],
+  }),
+  defineConfig({
     input: 'src/index.ts',
     external: ['rxjs', 'rxjs/operators', './scss/main.scss'],
     plugins: [dts()],
@@ -33,5 +53,8 @@ export default [
       file: `${name}.d.ts`,
       format: 'es',
     },
-  }
+  })
 ]
+
+
+export default configs;
